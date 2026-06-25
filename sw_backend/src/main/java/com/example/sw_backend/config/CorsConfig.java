3c5458d66d96efb6285c690a6a6a88ee2cef886c
Admin <Admin@DESKTOP-HGD186A>
2026-06-25 17:41:06 +0530

    add updated vercel link in corsconfig file in java

--- sw_backend/src/main/java/com/example/sw_backend/config/CorsConfig.java
+++ sw_backend/src/main/java/com/example/sw_backend/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://sw-portfolio-five.vercel.app")
                         .allowedMethods("*");
 
             }
