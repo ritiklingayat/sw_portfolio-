791d0fc4c3a1c57bc116a005503735f6327f11c8
Admin <Admin@DESKTOP-HGD186A>
2026-07-19 17:23:20 +0530

    Removed exposed key for Email.js TODO: remove other such keys

--- SW-Multimedia-main/src/services/email.js
+++ SW-Multimedia-main/src/services/email.js
@@ -1,14 +1,13 @@
 import emailjs from '@emailjs/browser';
 
-const SERVICE_ID = 'service_bonx91t';
-const CONTACT_TEMPLATE_ID = 'template_drqorul';
-const INTERNSHIP_TEMPLATE_ID = 'template_iz8zpqb';
-const PUBLIC_KEY = 'eZUUN-YkZXMludBNy';
 
 emailjs.init(PUBLIC_KEY);
 
 export async function sendContactEmail(templateParams) {
-  return emailjs.send(SERVICE_ID, CONTACT_TEMPLATE_ID, templateParams, PUBLIC_KEY);
+  const serviceID = import.meta.env.VITE_SERVICE_ID;
+  const contactTemplateID = import.meta.env.VITE_CONTACT_TEMPLATE_ID;
+  const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+  return emailjs.send(serviceID, contactTemplateID, templateParams, publicKey);
 }
 
 export async function sendInternshipEmail(templateParams) {
